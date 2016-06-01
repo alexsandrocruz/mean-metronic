5cfb3f3f875786fad0917b2bed8b5367fed7ca4e
John Smith <sittshein.app.developer@gmail.com>
2016-06-01 10:54:57 +0800

    reconcil

--- client/app/controllers/TodoController.ts
+++ client/app/controllers/TodoController.ts
@@ -1,10 +1,22 @@
-angular.module('MetronicApp').controller('TodoController', function($rootScope, $scope, $http, $timeout) {
-    $scope.$on('$viewContentLoaded', function() {   
-        App.initAjax(); // initialize core components        
-    });
+(function () {
+	
+	'use strict';
 
-    // set sidebar closed and body solid layout mode
-    $rootScope.settings.layout.pageContentWhite = true;
-    $rootScope.settings.layout.pageBodySolid = true;
-    $rootScope.settings.layout.pageSidebarClosed = true;
-});
+	var module = angular.module('MetronicApp');
+
+	var TodoController = function($rootScope, $scope, $http, $timeout) {
+
+		$scope.$on('$viewContentLoaded', function() {
+			App.initAjax(); // initialize core components        
+		});
+
+		// set sidebar closed and body solid layout mode
+		$rootScope.settings.layout.pageContentWhite = true;
+		$rootScope.settings.layout.pageBodySolid = true;
+		$rootScope.settings.layout.pageSidebarClosed = true;
+	}
+
+	module.controller('TodoController', TodoController);
+
+})(); 
+
